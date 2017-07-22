825bc5533e6646f9734e952c6577dea70bed3ec3
sxcw <yuan329419@gmail.com>
2017-07-21 21:21:23 -0500

    Index - Render sample text

--- index.android.js
+++ index.android.js
@@ -1,6 +1,11 @@
 // import a library to help create a component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+
 // create a component
+const App = () => (
+  <Text> Some text</Text>
+);
 
 // render it to the device
+AppRegistry.registerComponent('albums', () => App);
